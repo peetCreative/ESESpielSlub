fd0bb3e3f62be530e184d847b0a66d2235e3b3d9
Peter Klausing <s1856172@mail.zih.tu-dresden.de>
2014-10-07 18:09:39 +0200

    more libaries

--- src/madProgrammedRegisttrationApplication.java
+++ src/madProgrammedRegisttrationApplication.java
@@ -11,24 +11,52 @@ import javax.swing.JButton;
 import javax.swing.JTextPane;
 import javax.swing.JProgressBar;
 import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+
 import java.awt.GridBagLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Random;
 import java.awt.Font;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.Color;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.GridLayout;
 
 
 public class madProgrammedRegisttrationApplication {
 
+	private static final String Gelesen = "gelesen";
 	private JFrame frmMadprogrammtoregisterstudentsintoslubsystem;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JPanel RegelnPanel;
+	private JTextField LieblingsBuch;
+	private JTextField NameTextField;
+	private JTextField zihLogin;
 	private JTextField textField_3;
 	private JTextPane txtpnNameDeinesVaters;
+	private JProgressBar progressBar;
+	private JTextPane txtpnGibDeineDaten;
+	private int waitingTime = 300;
+	private JScrollPane scrollPane;
+	private String[] slubRegeln = {
+			"Die erste Regel der Slub ist du Redest nicht in der Slub!\n" ,
+					"Die zweite Regel der Slub ist du redest nicht in der Slub!\n" ,
+					"Die dritte Regel der Slub ist du redest nicht in der Slub\n" ,
+					"Die vierte Regel der Slub ist du sollst nicht mehr als 4 Bücher ausleihen!\n" ,
+					"Die fünfte Regel der Slub ist jegliche Beschädigung des Materials der Slub ist zu Ersätzen!\n" ,
+					"Die Sechste Regel der Slub ist das Personal ist beim Kaffee trinken nicht zu stören!\n" ,
+					"Die Siebente Regel der Slub ist du darfst diesen Haken nicht setzen!\n "
+	};
+	private JTextField txtDieserButtonLscht;
 
 	/**
 	 * Launch the application.
@@ -55,6 +83,7 @@ public class madProgrammedRegisttrationApplication {
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @param JTextPane() 
 	 */
 	private void initialize() {
 		frmMadprogrammtoregisterstudentsintoslubsystem = new JFrame();
@@ -63,6 +92,8 @@ public class madProgrammedRegisttrationApplication {
 		frmMadprogrammtoregisterstudentsintoslubsystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.MAGENTA);
+		panel.setForeground(Color.YELLOW);
 		frmMadprogrammtoregisterstudentsintoslubsystem.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new FormLayout(new ColumnSpec[] {
 				FormFactory.RELATED_GAP_COLSPEC,
@@ -103,9 +134,41 @@ public class madProgrammedRegisttrationApplication {
 		txtpnNameDeinesVaters.setText("Name der Tochter der Großmutter väterlicherseits");
 		panel_3.add(txtpnNameDeinesVaters);
 		
-		textField_1 = new JTextField();
-		panel_3.add(textField_1);
-		textField_1.setColumns(10);
+		NameTextField = new JTextField();
+		NameTextField.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				selectFocus();
+			}
+		});
+		NameTextField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e){
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				
+			}
+				//neuer Thread für Delay
+				//new DelayThread().start();
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				try {
+					Thread.sleep(waitingTime);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
+		panel_3.add(NameTextField);
+		NameTextField.setColumns(10);
 		
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2, "2, 4, fill, fill");
@@ -115,22 +178,173 @@ public class madProgrammedRegisttrationApplication {
 		txtpnLieblingsbuch.setText("Lieblingsbuch");
 		panel_2.add(txtpnLieblingsbuch);
 		
-		textField = new JTextField();
-		panel_2.add(textField);
-		textField.setColumns(10);
+		LieblingsBuch = new JTextField();
+		LieblingsBuch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				selectFocus();
+			}
+		});
+		LieblingsBuch.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e){
+				try {
+					Thread.sleep(waitingTime);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+				//neuer Thread für Delay
+				//new DelayThread().start();
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				try {
+					Thread.sleep(waitingTime);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+			
+		});
+		panel_2.add(LieblingsBuch);
+		LieblingsBuch.setColumns(10);
 		
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1, "2, 6, fill, fill");
 		
 		JTextPane txtpnErzhlMirMehr = new JTextPane();
+		txtpnErzhlMirMehr.setEditable(false);
 		txtpnErzhlMirMehr.setText("Erzähl mir mehr:");
 		panel_1.add(txtpnErzhlMirMehr);
 		
-		textField_2 = new JTextField();
-		panel_1.add(textField_2);
-		textField_2.setColumns(10);
+		zihLogin = new JTextField();
+		zihLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				selectFocus();
+			}
+		});
+		panel_1.add(zihLogin);
+		zihLogin.setColumns(10);
+		LieblingsBuch.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e){
+				try {
+					Thread.sleep(waitingTime);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+				//neuer Thread für Delay
+				//new DelayThread().start();
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				try {
+					Thread.sleep(waitingTime);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				LieblingsBuch.requestFocus();
+
+			}
+			
+			
+		});		
+		RegelnPanel = new JPanel();	
+		scrollPane = new JScrollPane(RegelnPanel);
+		RegelnPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("4dlu:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("4dlu:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("default:grow"),}));
+		ArrayList<JTextPane> RegelnTextPanes = new ArrayList<JTextPane>();
+		for (String regel: slubRegeln){
+			JTextPane pane = new JTextPane();
+			pane.setText(regel);
+			RegelnTextPanes.add(pane);
+		}
+		for (int i =0; i <  RegelnTextPanes.size(); i++){
+			RegelnPanel.add(RegelnTextPanes.get(i), 
+							 "2, " + (i*2 +2 ) + ",fill ,fill");
+			RegelnPanel.add(new JCheckBox("OK"), "4, " + (i*2 +2 ) + ",fill ,fill");
+		}
+		//JTextPane textPane = new JTextPane();
+		//textPane.setText("zweite Regel");
+		//RegelnPanel.add(textPane, "8, 2, fill, fill");
 		
-		JScrollPane scrollPane = new JScrollPane();
+		//JTextPane txtpnregel = new JTextPane();
+		//txtpnregel.setText("8.Regel");
+		//RegelnPanel.add(txtpnregel, "6, 4, fill, fill");
+		
+		//txtDieserButtonLscht = new JTextField();
+		//txtDieserButtonLscht.setText("Dieser Button löscht alle Eingaben");
+		//RegelnPanel.add(txtDieserButtonLscht, "2, 8, fill, default");
+		//txtDieserButtonLscht.setColumns(10);
+		
+		//JCheckBox chckbxOk = new JCheckBox("Ok");
+		//RegelnPanel.add(chckbxOk, "10, 10");
+		
+		///
+		//JTextPane textPane;
+		//JCheckBox checkBox = new JCheckBox();
+			//textPane = new JTextPane();
+			//textPane.setText(slubRegeln[0]);
+			//textPane.setBounds(8, 4 ,420,100);
+			//RegelnPanel.add(textPane);
+			//checkBox = new JCheckBox();
+			//checkBox.setSelected(false);
+			//checkBox.setText(Gelesen);
+			//RegelnPanel.add(checkBox);
+			
+		
+		
 		panel.add(scrollPane, "2, 8, fill, fill");
 		
 		JTextPane txtpnAchteAufVollstendigkeit = new JTextPane();
@@ -144,7 +358,7 @@ public class madProgrammedRegisttrationApplication {
 		JCheckBox chckbxNewCheckBox = new JCheckBox("ich habe die Regeln gelesen!");
 		panel.add(chckbxNewCheckBox, "2, 14");
 		
-		JProgressBar progressBar = new JProgressBar();
+		progressBar = new JProgressBar(SwingConstants.HORIZONTAL, 0,100);
 		panel.add(progressBar, "2, 16");
 		
 		JPanel panel_4 = new JPanel();
@@ -153,6 +367,7 @@ public class madProgrammedRegisttrationApplication {
 		panel.add(panel_4, "2, 18, fill, fill");
 		
 		JTextPane txtpnDeinPasswort = new JTextPane();
+		txtpnDeinPasswort.setEditable(false);
 		txtpnDeinPasswort.setText("Dein Passwort:");
 		panel_4.add(txtpnDeinPasswort);
 		
@@ -160,7 +375,7 @@ public class madProgrammedRegisttrationApplication {
 		panel_4.add(textField_3);
 		textField_3.setColumns(10);
 		
-		JTextPane txtpnGibDeineDaten = new JTextPane();
+		txtpnGibDeineDaten = new JTextPane();
 		txtpnGibDeineDaten.setEditable(false);
 		txtpnGibDeineDaten.setText("Gib deine Daten ein!");
 		panel.add(txtpnGibDeineDaten, "2, 20, fill, fill");
@@ -168,13 +383,15 @@ public class madProgrammedRegisttrationApplication {
 		JButton btnRegistrieren = new JButton("Registrieren");
 		btnRegistrieren.addActionListener(new RegisterListener());
 		panel.add(btnRegistrieren, "2, 22");
+		panel.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{NameTextField, LieblingsBuch, zihLogin, scrollPane, panel_3, txtpnNameDeinesVaters, panel_2, txtpnLieblingsbuch, txtpnDeinPasswort, panel_1, txtpnErzhlMirMehr, txtpnAchteAufVollstendigkeit, chckbxAllesRichtig, chckbxNewCheckBox, progressBar, panel_4, textField_3, txtpnGibDeineDaten, btnRegistrieren}));
 	}
+	
 	private class RegisterListener implements ActionListener
 	{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			String[] auswahl = {"0","o","O","i","1","l","j", "|", "!", "/","\\"} ;
+			String[] auswahl = {"0","o","O"};//,"i","1","l","j", "|"};//, "!", "/","\\"} ;
 			String password = "";
 			Random rand = new Random();
 			for( int i = 3; i < 10; i++){
@@ -182,7 +399,37 @@ public class madProgrammedRegisttrationApplication {
 			}
 			textField_3.setText(password);
 		}
-		
 	}
+	private class DelayThread extends Thread{
+
+		@Override
+		public void run() {
+			
+			progressBar.setValue(0);
+			for (int i= 0; i < 100; i += 10){
+				progressBar.setValue(i);
+				progressBar.repaint();
+				System.out.println(i);
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+			progressBar.setValue(0); }
+		}
+
+	public void selectFocus(){
+		switch(new Random().nextInt(3)){
+			case 0: NameTextField.requestFocus(); break; 
+			case 1: LieblingsBuch.requestFocus(); break;
+			case 2: zihLogin.requestFocus(); break;
+			
+		}
+	}
+		
 }
+
 
